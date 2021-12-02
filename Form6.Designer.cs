ff5c86b764ca9bcc0a5427ffcd7e60f4b7af5d78
kkobus2 <86433120+kkobus2@users.noreply.github.com>
2021-12-01 22:47:52 -0500

    Added Forms 6 and 7

--- Form6.Designer.cs
+++ Form6.Designer.cs
@@ -28,12 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.homeButton6 = new System.Windows.Forms.Button();
+            this.backButton6 = new System.Windows.Forms.Button();
+            this.helpButton6 = new System.Windows.Forms.Button();
+            this.saveButton6 = new System.Windows.Forms.Button();
+            this.cardInformation6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.phoneNumber6 = new System.Windows.Forms.Label();
+            this.address6 = new System.Windows.Forms.Label();
+            this.closestIntersection6 = new System.Windows.Forms.Label();
+            this.createAnAccount6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // homeButton6
+            // 
+            this.homeButton6.Location = new System.Drawing.Point(713, 12);
+            this.homeButton6.Name = "homeButton6";
+            this.homeButton6.Size = new System.Drawing.Size(75, 23);
+            this.homeButton6.TabIndex = 0;
+            this.homeButton6.Text = "Home";
+            this.homeButton6.UseVisualStyleBackColor = true;
+            // 
+            // backButton6
+            // 
+            this.backButton6.Location = new System.Drawing.Point(0, 12);
+            this.backButton6.Name = "backButton6";
+            this.backButton6.Size = new System.Drawing.Size(75, 23);
+            this.backButton6.TabIndex = 1;
+            this.backButton6.Text = "Back";
+            this.backButton6.UseVisualStyleBackColor = true;
+            // 
+            // helpButton6
+            // 
+            this.helpButton6.Location = new System.Drawing.Point(0, 415);
+            this.helpButton6.Name = "helpButton6";
+            this.helpButton6.Size = new System.Drawing.Size(75, 23);
+            this.helpButton6.TabIndex = 2;
+            this.helpButton6.Text = "Help";
+            this.helpButton6.UseVisualStyleBackColor = true;
+            // 
+            // saveButton6
+            // 
+            this.saveButton6.Location = new System.Drawing.Point(406, 415);
+            this.saveButton6.Name = "saveButton6";
+            this.saveButton6.Size = new System.Drawing.Size(75, 23);
+            this.saveButton6.TabIndex = 3;
+            this.saveButton6.Text = "Save";
+            this.saveButton6.UseVisualStyleBackColor = true;
+            // 
+            // cardInformation6
+            // 
+            this.cardInformation6.Location = new System.Drawing.Point(359, 292);
+            this.cardInformation6.Name = "cardInformation6";
+            this.cardInformation6.Size = new System.Drawing.Size(131, 23);
+            this.cardInformation6.TabIndex = 4;
+            this.cardInformation6.Text = "Card Information";
+            this.cardInformation6.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(376, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(254, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(376, 198);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(114, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(376, 163);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(114, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(376, 233);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(114, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // phoneNumber6
+            // 
+            this.phoneNumber6.AutoSize = true;
+            this.phoneNumber6.Location = new System.Drawing.Point(254, 166);
+            this.phoneNumber6.Name = "phoneNumber6";
+            this.phoneNumber6.Size = new System.Drawing.Size(81, 13);
+            this.phoneNumber6.TabIndex = 10;
+            this.phoneNumber6.Text = "Phone Number:";
+            // 
+            // address6
+            // 
+            this.address6.AutoSize = true;
+            this.address6.Location = new System.Drawing.Point(254, 198);
+            this.address6.Name = "address6";
+            this.address6.Size = new System.Drawing.Size(48, 13);
+            this.address6.TabIndex = 11;
+            this.address6.Text = "Address:";
+            // 
+            // closestIntersection6
+            // 
+            this.closestIntersection6.AutoSize = true;
+            this.closestIntersection6.Location = new System.Drawing.Point(254, 236);
+            this.closestIntersection6.Name = "closestIntersection6";
+            this.closestIntersection6.Size = new System.Drawing.Size(105, 13);
+            this.closestIntersection6.TabIndex = 12;
+            this.closestIntersection6.Text = "Closest Intersection: ";
+            // 
+            // createAnAccount6
+            // 
+            this.createAnAccount6.AutoSize = true;
+            this.createAnAccount6.Location = new System.Drawing.Point(373, 31);
+            this.createAnAccount6.Name = "createAnAccount6";
+            this.createAnAccount6.Size = new System.Drawing.Size(97, 13);
+            this.createAnAccount6.TabIndex = 13;
+            this.createAnAccount6.Text = "Create An Account";
+            // 
+            // Form6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.createAnAccount6);
+            this.Controls.Add(this.closestIntersection6);
+            this.Controls.Add(this.address6);
+            this.Controls.Add(this.phoneNumber6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cardInformation6);
+            this.Controls.Add(this.saveButton6);
+            this.Controls.Add(this.helpButton6);
+            this.Controls.Add(this.backButton6);
+            this.Controls.Add(this.homeButton6);
+            this.Name = "Form6";
             this.Text = "Form6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button homeButton6;
+        private System.Windows.Forms.Button backButton6;
+        private System.Windows.Forms.Button helpButton6;
+        private System.Windows.Forms.Button saveButton6;
+        private System.Windows.Forms.Button cardInformation6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label phoneNumber6;
+        private System.Windows.Forms.Label address6;
+        private System.Windows.Forms.Label closestIntersection6;
+        private System.Windows.Forms.Label createAnAccount6;
     }
 }
--- Form7.cs
+++ Form7.cs
@@ -16,5 +16,10 @@ namespace Software_Group_Project
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
